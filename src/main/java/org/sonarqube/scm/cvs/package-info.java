a891165adfd6f60c58deb891b0b3d90cc2a739d4
Simon Brandhof <simon.brandhof@sonarsource.com>
2018-04-09 20:07:12 +0200

    SONARSCCVS-5 Rename packages to org.sonarqube.scm.cvs

--- src/main/java/org/sonarqube/scm/cvs/package-info.java
+++ src/main/java/org/sonarqube/scm/cvs/package-info.java
@@ -18,7 +18,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 @ParametersAreNonnullByDefault
-package org.sonar.plugins.scm.cvs;
+package org.sonarqube.scm.cvs;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
